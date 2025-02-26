46394712955a618cb9b66975a95702dd8305273b
p0mi <73187612+p0mi@users.noreply.github.com>
2025-02-26 12:14:16 +0000

    new file:   trace.zip 	modified:   wright/test_1.py 	modified:   wright/trace.zip

--- wright/test_1.py
+++ wright/test_1.py
@@ -3,17 +3,17 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
 
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
 
     page = context.new_page()
-    page.goto("https://guu.ru/")
-    page.get_by_role("link", name="Об университете").click()
-    page.get_by_role("link", name="Поступающим").click()
-    page.get_by_role("link", name="О целевом обучении").click()
-    page.get_by_role("link", name="Правила приёма на 2025/2026г").click()
+    page.goto("https://probable-disco-5567x7577w4c4rw-5173.app.github.dev/")
+    # page.get_by_role("link", name="Об университете").click()
+    # page.get_by_role("link", name="Поступающим").click()
+    # page.get_by_role("link", name="О целевом обучении").click()
+    # page.get_by_role("link", name="Правила приёма на 2025/2026г").click()
 
     context.tracing.stop(path="trace.zip")
 
